a5306329d0436da09c3dfc0cf8640962220eaccd
deepali <deepalia@hcl.com>
2019-12-12 13:37:03 +0530

    Install Folder Issue resolved

--- core/src/main/java/com/hcl/ecomm/core/config/LoginServiceConfig.java
+++ core/src/main/java/com/hcl/ecomm/core/config/LoginServiceConfig.java
@@ -1,46 +1,26 @@
 package com.hcl.ecomm.core.config;
 
 import org.osgi.service.metatype.annotations.AttributeDefinition;
-import org.osgi.service.metatype.annotations.AttributeType;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
-@ObjectClassDefinition(name="Login Service Configuration")
+@ObjectClassDefinition(name = "ABCD", description = "Component Usage Report Configurations")
 public @interface LoginServiceConfig {
 
 	String servicePathDefaultValue = "/V1/integration/admin/token";
 	String domainNameDefaultValue = "localhost:8081/magento/rest";
 	String userNameDefaultValue = "admin";
 	String passwordDefaultValue = "Admin123";
-	
-	@AttributeDefinition(
-			name="DOMAIN_NAME",
-			description="This is domain name",
-			defaultValue=domainNameDefaultValue,
-			type=AttributeType.STRING
-			)	
+
+	@AttributeDefinition(name = "DOMAIN_NAME", description = "This is domain name", defaultValue = {domainNameDefaultValue} )
 	String loginservice_domainName_string() default domainNameDefaultValue;
 	
-	@AttributeDefinition(
-			name="SERVICE_PATH",
-			description="This is the API path",
-			defaultValue=servicePathDefaultValue,
-			type=AttributeType.STRING
-			)
+	@AttributeDefinition(name = "SERVICE_PATH", description = "This is the API path", defaultValue = {servicePathDefaultValue} )
 	String loginservice_servicePath_string() default servicePathDefaultValue;
-	
-	@AttributeDefinition(
-			name="USERNAME",
-			description="USERNAME",
-			defaultValue=userNameDefaultValue,
-			type=AttributeType.STRING
-			)	
+
+	@AttributeDefinition(name = "USERNAME", description = "USERNAME", defaultValue = {userNameDefaultValue} )
 	String loginservice_username_string() default userNameDefaultValue;
-	
-	@AttributeDefinition(
-			name="PASSWORD",
-			description="PASSWORD",
-			defaultValue=passwordDefaultValue,
-			type=AttributeType.STRING
-			)	
+
+	@AttributeDefinition(name = "PASSWORD", description = "PASSWORD", defaultValue ={ passwordDefaultValue} )
 	String loginservice_password_string() default passwordDefaultValue;
+
 }
--- core/src/main/java/com/hcl/ecomm/core/services/impl/LoginServiceImpl.java
+++ core/src/main/java/com/hcl/ecomm/core/services/impl/LoginServiceImpl.java
@@ -3,7 +3,9 @@ package com.hcl.ecomm.core.services.impl;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.propertytypes.ServiceDescription;
+import org.osgi.service.component.propertytypes.ServiceRanking;
+import org.osgi.service.component.propertytypes.ServiceVendor;
 import org.osgi.service.metatype.annotations.Designate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,38 +14,36 @@ import com.hcl.ecomm.core.config.LoginServiceConfig;
 import com.hcl.ecomm.core.services.LoginService;
 
 
-@Component(
-		enabled=true,
-		immediate=true,
-		name="Login Service Configuration structure",
-		service = LoginService.class,
-		configurationPolicy = ConfigurationPolicy.REQUIRE
-		)
+
+
+@Component(service = {LoginService.class})
 @Designate(ocd = LoginServiceConfig.class)
+
+@ServiceDescription("Login service")
+@ServiceRanking(1001)
+@ServiceVendor("Shubham Mittal")
+
 public class LoginServiceImpl implements LoginService{
 
 	private static final Logger LOG = LoggerFactory.getLogger(LoginServiceImpl.class);	
-		
-	private LoginServiceConfig config;
 	
-	@Activate
-	protected void activate(LoginServiceConfig config) {
-		this.config=config;
-	}
+	 @Activate
+	 private LoginServiceConfig config;
+
 	@Override
 	public String getDomainName() {		
-		return config.loginservice_domainName_string();
+		return this.config.loginservice_domainName_string();
 	}
 	@Override
 	public String getServicePath() {
-		return config.loginservice_servicePath_string();
+		return this.config.loginservice_servicePath_string();
 	}
 	@Override
 	public String getUsername() {
-		return config.loginservice_username_string();
+		return this.config.loginservice_username_string();
 	}
 	@Override
 	public String getPassword() {
-		return config.loginservice_password_string();
+		return this.config.loginservice_password_string();
 	}
 }
